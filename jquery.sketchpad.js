16c8a12508ca80603b2ec73eea2b218962c33663
张超凡 <gagazcfan@icloud.com>
2016-08-13 22:53:45 +0800

    support create many sketchpad in one page

--- jquery.sketchpad.js
+++ jquery.sketchpad.js
@@ -1,70 +1,73 @@
 (function($){
 
-  function createCanvas(width, height) {
-    var canvas = $('<canvas></canvas>');
-    canvas[0].width = width;
-    canvas[0].height = height;
-    return canvas;
+  function SketchPad(canvas) {
+    this.canvas    = canvas;
+    this.ctx       = canvas.getContext('2d');
+    this.isDrawing = false;
+    this.offset    = {
+      left: canvas.offsetLeft,
+      top:  canvas.offsetTop
+    };
+    this.prevX = this.prevY = this.currX = this.currY = 0;
+
+    canvas.onmousedown = this.mousedown.bind(this);
+    canvas.onmouseup   = this.mouseup.bind(this);
+    canvas.onmousemove = this.mousemove.bind(this);
   }
 
+  // calculate the canvas local postion
+  SketchPad.prototype.calXY = function (e) {
+    return {
+      x: e.clientX - this.offset.left,
+      y: e.clientY - this.offset.top
+    };
+  }
+
+  SketchPad.prototype.mousedown = function (e) {
+    var pos = this.calXY(e);
+    this.prevX = pos.x;
+    this.prevY = pos.y;
+    this.isDrawing = true;
+  }
+
+  SketchPad.prototype.mouseup = function (e) {
+    this.isDrawing = false;
+  }
+
+  SketchPad.prototype.mousemove = function (e) {
+    if (!this.isDrawing) return;
+    var pos = this.calXY(e);
+    this.currX = pos.x;
+    this.currY = pos.y;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(this.prevX, this.prevY);
+    this.ctx.lineTo(this.currX, this.currY);
+    this.ctx.strokeStyle = '#000';
+    this.ctx.lineWidth = 2;
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    this.prevX = this.currX;
+    this.prevY = this.currY;
+  }
+
+  // plugin entrance
   $.fn.sketchpad = function() {
     this.css('border', '1px solid #000');
-    var canvas = createCanvas(this.width(), this.height());
-    var ctx = canvas[0].getContext('2d');
-    this.append(canvas);
 
-    var isDrawing = false;
-    var offset = {
-      left: this[0].offsetLeft,
-      top:  this[0].offsetTop
-    }
-    var pos
-    var prevX = 0,
-        prevY = 0,
-        currX = 0,
-        currY = 0;
+    // create sketchpads for every single element matched
+    var sketchPads = [];
+    this.each(function(index, el) {
+      var canvasDom = $('<canvas></canvas>')[0];
+      canvasDom.width = $(this).width();
+      canvasDom.height = $(this).height();
+      $(this).append(canvasDom);
+      sketchPads[index] = new SketchPad(canvasDom);
+    });
 
-    canvas.mousedown(mousedown);
-    canvas.mouseup(mouseup);
-    canvas.mousemove(mousemove);
-
-    function calXY(e) {
-      return {
-        x: e.clientX - offset.left,
-        y: e.clientY - offset.top
-      };
-    }
-
-    function mousedown(e) {
-      var pos = calXY(e);
-      prevX = pos.x;
-      prevY = pos.y;
-      isDrawing = true;
-    }
-
-    function mouseup(e) {
-      isDrawing = false;
-    }
-
-    function mousemove(e) {
-      if (!isDrawing) return;
-      var pos = calXY(e);
-      currX = pos.x;
-      currY = pos.y;
-
-      ctx.beginPath();
-      ctx.moveTo(prevX, prevY);
-      ctx.lineTo(currX, currY);
-      ctx.strokeStyle = '#000';
-      ctx.lineWidth = 2;
-      ctx.stroke();
-      ctx.closePath();
-
-      prevX = currX;
-      prevY = currY;
-    }
-
-    return this;
+    // return array of sketchpad if matched multi elements
+    return sketchPads.length === 1?sketchPads[0]:sketchPads;
   };
     
 }(jQuery));
